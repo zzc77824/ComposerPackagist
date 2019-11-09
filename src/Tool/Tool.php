e2289130d05b3bc95881d7ae766515371548da96
zhangzc <xizaoqu@zhangzc.top>
2019-11-09 09:14:41 +0800

    生成随机字符串

--- src/Tool/Tool.php
+++ src/Tool/Tool.php
@@ -7,6 +7,9 @@ namespace Tool;
 
 class Tool
 {
+    /**
+     * @return mixed|string 返回客户端IP
+     */
     public function getIP()
     {
         if (getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
@@ -18,8 +21,28 @@ class Tool
         } else if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
             $ip = $_SERVER['REMOTE_ADDR'];
         }
-        $res =  preg_match ( '/[\d\.]{7,15}/', $ip, $matches ) ? $matches [0] : '';
+        $res = preg_match('/[\d\.]{7,15}/', $ip, $matches) ? $matches [0] : '';
 
         return $res;
+    }
+
+    /*
+    * 生成随机字符串
+    * @param int $length 生成随机字符串的长度
+    * @param string $char 组成随机字符串的字符串
+    * @return string $string 生成的随机字符串
+    */
+    function getRandStr($length = 32, $char = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+        if(!is_int($length) || $length < 0) {
+            return false;
+        }
+
+        $string = '';
+
+        for($i = $length; $i > 0; $i--) {
+            $string .= $char[mt_rand(0, strlen($char) - 1)];
+        }
+
+        return $string;
     }
 }
